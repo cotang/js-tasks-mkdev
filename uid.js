cc2dcf3f58cb980cb9d52b288dc992a167e09b4f
Konstantin Bogdanov <kbogdanov1@gmail.com>
2018-07-04 22:37:19 +0300

    uid - into 2 functions, loop change

--- uid.js
+++ uid.js
@@ -1,17 +1,16 @@
-function uid(alphabet, n) {
-  var uid = '';
-  for (var i=0;i<n;i++){
-    var rand = Math.random() * alphabet.length;
-    rand = Math.floor(rand);
-    uid += alphabet[rand]
-  }
-  return uid
+function pickRandomLetter(str) {
+  var rand = Math.random() * str.length;
+  rand = Math.floor(rand);
+  return str[rand];
 }
 
-// uid("ab", 1) // One of: "a", "b"
-// uid("ab", 2) // One of: "aa", "ab", "ba", "bb"
-// uid("ab", 3) // One of: "aaa", "aab", "aba" ...
+function uid(alphabet, n) {
+  return Array(n).fill(null).map(() => pickRandomLetter(alphabet)).join('')
+}
 
-// uid("abc", 1) // One of: "a", "b", "c"
-// uid("abc", 2) // One of: "aa", "ab", "ac", "ba", "bb", "bc", "ca", "cb", "cc"
-// uid("abc", 3) // One of: "aaa", "aab", "aac" ...
+
+// uid("abc", 0) 
+// uid("abc", 1) 
+// uid("abc", 2)
+// uid("abc", 3)
+// uid("abcde", 27)
