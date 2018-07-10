f05a248cebb89584161f4f47d6cc06ceb685cd81
Konstantin Bogdanov <kbogdanov1@gmail.com>
2018-07-10 23:26:51 +0300

    immutableMerge cor

--- immutableMerge.js
+++ immutableMerge.js
@@ -9,23 +9,16 @@ var o32 = { age: 40, contact: { email: 'baa@example.com', tel: { foo: 'baz'} }}
 
 
 function immutableMerge(obj1, obj2){
-  // version 1
   var clone = {};
   for (var i=0;i<arguments.length;i++){
     for (var key in arguments[i]) {
       clone[key] = arguments[i][key];
     }
-  }
-
-  // version 2
-  // var clone = Object.assign(parseStringify(obj1), parseStringify(obj2));
-  
+  }  
   return clone;
 }
 
-function parseStringify(obj){
-  return JSON.parse(JSON.stringify(obj));
-}
+
 
 immutableMerge(o11, o12);
 immutableMerge(o21, o22);
