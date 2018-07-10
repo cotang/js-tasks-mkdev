a44ac7dcc773666b3aa9e6ae42a7413a1dd49215
Konstantin Bogdanov <kbogdanov1@gmail.com>
2018-07-10 23:43:08 +0300

    immutableMerge cor2

--- immutableMerge.js
+++ immutableMerge.js
@@ -4,22 +4,22 @@ var o12 = { age: 40 }
 var o21 = { name: 'fred', age: 10, contact: { email: 'moo@example.com' }};
 var o22 = { age: 40, contact: { email: 'baa@example.com' }}
 
-var o31 = { name: 'fred', age: 10, contact: { email: 'moo@example.com', tel: { foo: 'bar'} }};
-var o32 = { age: 40, contact: { email: 'baa@example.com', tel: { foo: 'baz'} }}
+var o31 = { name: 'fred', age: 10, contact: { email: 'moo@example.com', tel: { mobile: 112} }};
+var o32 = { age: 40, contact: { email: 'baa@example.com', tel: { mobile: 911} }}
+var o33 = { name: 'matthew', surname: 'perry', contact: { tel: { mobile: 000} }}
 
 
-function immutableMerge(obj1, obj2){
+function immutableMerge(...arguments){
   var clone = {};
-  for (var i=0;i<arguments.length;i++){
-    for (var key in arguments[i]) {
-      clone[key] = arguments[i][key];
+  for (let argument of arguments)
+    for (let key in argument) {
+      clone[key] = argument[key];
     }
   }  
   return clone;
 }
 
 
-
 immutableMerge(o11, o12);
 immutableMerge(o21, o22);
-immutableMerge(o31, o32);
+immutableMerge(o31, o32, o33);
