7b39f5d01631677e06855cecbe392c95afb89df3
Konstantin Bogdanov <kbogdanov1@gmail.com>
2018-07-31 11:27:57 +0300

    express3 v2

--- express3/client/components/App.jsx
+++ express3/client/components/App.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+// import logo2 from "logo2.svg"
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Hello Big Bright World</h1>
-        <img id="logo" alt="image" />
+        <img src="public/logo.jpg" alt="logo1" />
+        {/* <img src={logo2} alt="logo2" /> */}
       </div>
     );
   }
--- express3/server/index.js
+++ express3/server/index.js
@@ -4,14 +4,15 @@ var path = require('path');
 var app = express();
 
 
-app.use(express.static(__dirname +'./../'));
-// app.use("/public", express.static(path.resolve(__dirname, '../public')));
+app.use(express.static(__dirname +'./../public'));
+app.use('/public', express.static(path.resolve(__dirname, '../public')));
 
-app.get('/', function(req, res) {
-	res.sendFile(__dirname+'/index.html');
+app.get('*', function(req, res) {
+	res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 app.listen(3000);
+
 
 // errors
 app.use(function(err, req, res, next) {
