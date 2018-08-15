46019625b75dc6d18d2ae73a8838052c8c3d538d
Konstantin Bogdanov <kbogdanov1@gmail.com>
2018-08-15 22:30:27 +0300

    extra files deleted

--- express-react-mongo/client/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App.jsx';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
-
-render(<App />,document.getElementById('root'));
--- express-react-old2/README.md
+++
@@ -1,6 +0,0 @@
-Install dependencies <br>
-`npm i`
-
-Run webpack <br>
-`npm run webpack` <br>
-`npm start`
